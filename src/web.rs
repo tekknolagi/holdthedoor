0b615610c18edfe45fb322a4b1a800c53922a160
Maxwell Bernstein <tekknolagi@gmail.com>
2017-04-23 18:29:35 +0200

    Shitty workaround for Person form

--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,6 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
+#![allow(dead_code)]
 
 #[macro_use]
 extern crate serde_derive;
@@ -8,7 +9,9 @@ extern crate rocket;
 extern crate serde_json;
 extern crate rocket_contrib;
 
+use rocket::request::Form;
 use rocket_contrib::Template;
+use std::collections::HashMap;
 
 mod util;
 
@@ -18,12 +21,42 @@ fn index() -> String {
     format!("{:#?}", db.people[0])
 }
 
-#[get("/rules")]
+#[get("/")]
 fn rules_list() -> Template {
     let db = util::Db::open("db.json");
     Template::render("rules/list", &db)
 }
 
+#[get("/")]
+fn people_list() -> Template {
+    let db = util::Db::open("db.json");
+    Template::render("people/list", &db)
+}
+
+#[get("/add")]
+fn people_add() -> Template {
+    let ctx = HashMap::<String,String>::new();
+    Template::render("people/add", &ctx)
+}
+
+#[derive(FromForm)]
+struct PersonForm { id: u64, name: String }
+impl PersonForm {
+    pub fn to_person(&self) -> util::Person {
+        util::Person { id: self.id, name: self.name.clone() }
+    }
+}
+
+#[post("/add", data="<user_input>")]
+fn post_people_add(user_input: Form<PersonForm>) -> String {
+    let input: util::Person = user_input.into_inner().to_person();
+    format!("{:#?}", input)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, rules_list]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/rules", routes![rules_list])
+        .mount("/people", routes![people_list, people_add, post_people_add])
+        .launch();
 }
